82a9cd697311a8e43bf3b3494ba5f3d47c538c28
Jason Lattimer <jason.lattimer@gmail.com>
2019-03-27 11:49:59 +0100

    Version number update

--- D365AppInsights.Js.NuGet/Properties/VersionInfo.cs
+++ D365AppInsights.Js.NuGet/Properties/VersionInfo.cs
@@ -1,10 +1,10 @@
 using System.Reflection;
 
 // This should be the same version as below
-[assembly: AssemblyFileVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.4.0")]
 
 #if DEBUG
-[assembly: AssemblyInformationalVersion("1.0.3-PreRelease")]
+[assembly: AssemblyInformationalVersion("1.0.4-PreRelease")]
 #else
-[assembly: AssemblyInformationalVersion("1.0.3")]
+[assembly: AssemblyInformationalVersion("1.0.4")]
 #endif
